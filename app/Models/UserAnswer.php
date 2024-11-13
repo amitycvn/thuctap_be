91b42378f966595815bc9f6aab5b6c37b66924bc
amitycvn <c.amity.vn@gmail.com>
2024-11-13 22:11:04 +0700

    insert data in result table

--- app/Models/UserAnswer.php
+++ app/Models/UserAnswer.php
@@ -6,17 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserAnswer extends Model
 {
-    protected $fillable = ['id','UserTestID','QuestionID','SelectedOption','TextAnswer'];
+    protected $fillable = ['id', 'UserTestID', 'QuestionID', 'SelectedOption', 'TextAnswer'];
 
     // Quan hệ với bảng questions (mỗi user_answer thuộc về một câu hỏi)
     public function userTest()
-{
-    return $this->belongsTo(UserTest::class, 'UserTestID');
-}
+    {
+        return $this->belongsTo(UserTest::class, 'UserTestID');
+    }
 
-public function question()
-{
-    return $this->belongsTo(Question::class, 'QuestionID');
-}
-
+    public function question()
+    {
+        return $this->belongsTo(Question::class, 'QuestionID');
+    }
 }
